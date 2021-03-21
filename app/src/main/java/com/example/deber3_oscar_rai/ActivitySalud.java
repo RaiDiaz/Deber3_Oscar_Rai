def9500ada6a8fd85e73d0efc67f8ebabf006273
matthewraidiaz <raidiaz1941@gmail.com>
2021-03-20 20:45:32 -0500

    hola

--- app/src/main/java/com/example/deber3_oscar_rai/ActivitySalud.java
+++ app/src/main/java/com/example/deber3_oscar_rai/ActivitySalud.java
@@ -2,13 +2,29 @@ package com.example.deber3_oscar_rai;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.EditText;
+
+import android.widget.Toast;
+
 
 public class ActivitySalud extends AppCompatActivity {
+
+    private ItemList listaSalud;
+    private EditText descripcion;
+    private EditText valor;
+
+    public ActivitySalud() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +32,10 @@ public class ActivitySalud extends AppCompatActivity {
         setContentView(R.layout.activity_salud);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        listaSalud=new ItemList();
+        descripcion= findViewById(R.id.edit_descripcion);
+        valor= findViewById(R.id.edit_valor);
+
     }
 
     @Override
@@ -23,4 +43,17 @@ public class ActivitySalud extends AppCompatActivity {
         startActivity(new Intent(ActivitySalud.this,MainActivity.class));
         return super.onOptionsItemSelected(item);
     }
+
+    public void addItem(View v){
+        if(TextUtils.isEmpty(descripcion.getText().toString())&&TextUtils.isEmpty(valor.getText().toString())){
+            Toast toast = Toast.makeText(this, "Por favor llene todos los campos",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else{
+            listaSalud.addItem(descripcion.getText().toString(),Double.parseDouble(valor.getText().toString()));
+        }
+    }
+
+
 }
